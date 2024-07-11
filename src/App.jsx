9b83ef3cce561b1d803a6f0d80dd1dd16314738d
rajesh <rajesh.t7982@gmail.com>
2024-07-11 11:05:28 +0530

    client

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+import { useState } from "react";
+import React from "react";
+
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    let res = await fetch("/api/users");
+    console.log("res", res);
+    let usersList = await res.json();
+    setUsers(usersList.payload);
+  };
+
+  //useEffect(()=>{getUsers()},[])
+
+  return (
+    <>
+      <button onClick={getUsers}>Users</button>
+      {users.map((user) => (
+        <div key={user.id}>
+          <h1>{user.id}</h1>
+          <h1>{user.name}</h1>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default App;
