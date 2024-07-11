68cdfe5ec870cfc8b4b5e23a25b5621f9ce4ca85
rajesh <rajesh.t7982@gmail.com>
2024-07-11 11:29:16 +0530

    client-v4

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
   server: {
     proxy: {
       // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
-      "/api": {
-        target: "https://mern-deploy-backend-3e92w1hwd-rajeshthappetas-projects.vercel.app",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+    //  "/api": {
+   //     target: "https://mern-deploy-backend-3e92w1hwd-rajeshthappetas-projects.vercel.app/",
+   //     changeOrigin: true,
+   //     rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
