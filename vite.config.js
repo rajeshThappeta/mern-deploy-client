1a3240b0c71be692d7f5d5e17f5d4ac2ae2f3d23
rajesh <rajesh.t7982@gmail.com>
2024-07-12 10:38:15 +0530

    1

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
-    //  "/api": {
-   //     target: "https://mern-deploy-backend-3e92w1hwd-rajeshthappetas-projects.vercel.app/",
-   //     changeOrigin: true,
-   //     rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
-});
+  plugins: [react()],
+})
